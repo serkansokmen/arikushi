23311e5fc987f4d75ce42b61dd472ffe5f65dd22
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2016-01-28 19:55:03 +0200

    added basic tests

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ var mapping = require('./mapping.json').mapping;
 module.exports = {
   mapping: mapping,
   convert: function(str) {
+
+    if (str === null || (str != null && typeof str !== 'string')) {
+      return '';
+    }
     var result = '';
     for (var i = 0; i < str.length; i++) {
       var char = str.charAt(i).toUpperCase();
